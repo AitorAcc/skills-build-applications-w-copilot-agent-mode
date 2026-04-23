1411bf0b9768371681a15b6cf42654f123aa2b77
AitorAcc <aitor.cuadrado@accenture.com>
2026-04-23 08:46:42 +0000

    Step 3: Initialize

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,71 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from djongo import models
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Activity(models.Model):
+    user = models.CharField(max_length=100)
+    activity_type = models.CharField(max_length=100)
+    duration = models.IntegerField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Leaderboard(models.Model):
+    user = models.CharField(max_length=100)
+    points = models.IntegerField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    difficulty = models.CharField(max_length=50)
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        # Clear collections
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create users
+        users = [
+            User.objects.create_user(username='ironman', email='ironman@marvel.com', password='pass', first_name='Tony', last_name='Stark'),
+            User.objects.create_user(username='captainamerica', email='cap@marvel.com', password='pass', first_name='Steve', last_name='Rogers'),
+            User.objects.create_user(username='batman', email='batman@dc.com', password='pass', first_name='Bruce', last_name='Wayne'),
+            User.objects.create_user(username='wonderwoman', email='wonderwoman@dc.com', password='pass', first_name='Diana', last_name='Prince'),
+        ]
+
+        # Create activities
+        Activity.objects.create(user='ironman', activity_type='Run', duration=30)
+        Activity.objects.create(user='captainamerica', activity_type='Swim', duration=45)
+        Activity.objects.create(user='batman', activity_type='Cycle', duration=60)
+        Activity.objects.create(user='wonderwoman', activity_type='Yoga', duration=50)
+
+        # Create leaderboard
+        Leaderboard.objects.create(user='ironman', points=100)
+        Leaderboard.objects.create(user='captainamerica', points=90)
+        Leaderboard.objects.create(user='batman', points=95)
+        Leaderboard.objects.create(user='wonderwoman', points=98)
+
+        # Create workouts
+        Workout.objects.create(name='Pushups', difficulty='Easy')
+        Workout.objects.create(name='Pullups', difficulty='Medium')
+        Workout.objects.create(name='Squats', difficulty='Hard')
+        Workout.objects.create(name='Plank', difficulty='Medium')
+
+        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data.'))
---
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -0,0 +1,80 @@
+"""octofit_tracker URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers, serializers, viewsets
+from django.contrib.auth import get_user_model
+from .management.commands.populate_db import Team, Activity, Leaderboard, Workout
+
+# Serializers
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = ['id', 'name']
+
+class ActivitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Activity
+        fields = ['id', 'user', 'activity_type', 'duration']
+
+class LeaderboardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Leaderboard
+        fields = ['id', 'user', 'points']
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Workout
+        fields = ['id', 'name', 'difficulty']
+
+# ViewSets
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+class ActivityViewSet(viewsets.ModelViewSet):
+    queryset = Activity.objects.all()
+    serializer_class = ActivitySerializer
+
+class LeaderboardViewSet(viewsets.ModelViewSet):
+    queryset = Leaderboard.objects.all()
+    serializer_class = LeaderboardSerializer
+
+class WorkoutViewSet(viewsets.ModelViewSet):
+    queryset = Workout.objects.all()
+    serializer_class = WorkoutSerializer
+
+# Routers
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'teams', TeamViewSet)
+router.register(r'activities', ActivityViewSet)
+router.register(r'leaderboard', LeaderboardViewSet)
+router.register(r'workouts', WorkoutViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+]
